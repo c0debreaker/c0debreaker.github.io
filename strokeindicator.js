3fffecf5adfa2540dbad11d7809ca4732bcd8758
Ronneil Camara <rcamara@rocketmail.com>
2015-10-28 23:41:14 -0500

    stroke indicator

---
+++ strokeindicator.js
@@ -0,0 +1,40 @@
+alert("🍃🍃🍃Stroke has a new indicator🍃🍃🍃"+"\n\n"+
+"Share this message so you can save one's life. "+"\n\n"+
+"Will you send this along?"+"\n\n"+
+"Blood Clots/Stroke - They Now Have a Fourth Indicator, the Tongue");
+
+alert("🍃I will continue to forward this every time it comes around!"+"\n\n"+
+"STROKE: Remember the 1st Three Letters...."+"\n\n"+
+"🍃 S. T. R. 🍃"+"\n");
+
+alert("STROKE IDENTIFICATION:"+"\n\n"+
+"🍃Senario :- During a BBQ, a woman stumbled and took a little fall - she assured everyone that she was fine (they offered to call paramedics) ...she said she had just tripped over a brick because of her new shoes."+"\n"+
+"They got her cleaned up and got her a new plate of food. While she appeared a bit shaken up, Jane went about enjoying herself the rest of the evening."+"\n\n"+
+"🍃Jane's husband called later telling everyone that his wife had been taken to the hospital - (at 6:00 PMJane passed away) "+"\n"+
+"She had suffered a stroke at the BBQ. "+"\n"+
+"Had they known how to identify the signs of a stroke, perhaps Jane would be with us today. Some don't die. "+"\n"+
+"They end up in a helpless, hopeless condition instead."+"\n\n"+
+"🍃It only takes a minute to read this."+"\n\n"+
+"🍃A neurologist says that if he can get to a stroke victim within 3 hours he can totally reverse the effects of a stroke...totally. "+"\n"+
+"He said the trick was getting a stroke recognized, diagnosed, and then getting the patient medically cared for within 3 hours, which is tough.");
+
+alert("🍃RECOGNIZING A STROKE"+"\n\n"+
+"Thank God for the sense to remember the '3' steps, STR. Read and"+"\n"+
+"Learn!"+"\n\n"+
+"🍃Sometimes symptoms of a stroke are difficult to identify. "+"\n"+
+"Unfortunately, the lack of awareness spells disaster. The stroke victim may suffer severe brain damage when people nearby fail to recognize the symptoms of a stroke."+"\n"+
+"Now doctors say a bystander can recognize a stroke by asking three simple questions:"+"\n\n"+
+"✅S *Ask the individual to SMILE."+"\n\n"+
+"✅T *Ask the person to TALK and SPEAK A");
+
+alert("SIMPLE SENTENCE (Coherently)"+"\n"+
+"(i.e. Chicken Soup)"+"\n"+
+"✅R *Ask him or her to RAISE BOTH ARMS."+"\n\n"+
+"🍃If he or she has trouble with ANY ONE of these tasks, call emergency number immediately and describe the symptoms to the dispatcher."+"\n\n");
+alert("🍃New Sign of a Stroke ---"+"\n\n"+
+"✅Stick out Your Tongue!"+"\n\n"+
+"NOTE: Another 'sign' of a stroke is this: Ask the person to 'stick' out his tongue. If the tongue is"+"\n"+
+"'crooked', if it goes to one side or the other that is also an indication of a stroke."+"\n"+
+"✅A cardiologist says if everyone who gets this e-mail sends it to 10 people; you can bet that at least one life will be saved."+"\n"+
+"💖💖💖💖💖💖💖💖💖"+"\n"+
+"✅I have done my part. Will you😊");
